0171e56c4a62cf5090e5fa8f0fc23038f353dfe8
aajy <aajy.000@gmail.com>
2024-03-01 23:07:19 +0900

    리덕스 개발자도구 적용하기

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './todoSlice';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
-const store = configureStore({
-	reducer: {},
-});
-
-export default store;
+export const store = configureStore(
+	{
+		reducer: {
+			todos: todoReducer,
+		},
+	},
+	devToolsEnhancer()
+);
